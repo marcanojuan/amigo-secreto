95748676a60080df07ff5af070764f85a19852f7
marcanojuan <marcano_juan@proton.me>
2025-01-28 16:28:06 -0400

    Implementación función agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+
+function addFriends() {
+    const elementInput = document.getElementById('amigo');
+    const friendName = elementInput.value.trim();
+
+    if (!isValidInput(friendName)) {
+        alert('Por favor, ingrese un nombre valido.');
+        return;
+    }
+
+    friends.push(friendName);
+    elementInput.value = '';
+    updateFriendsList();
+    return;
+}
+
+function isValidInput(input) {
+    // Regex para validar que el input sea un string con solo letras y espacios.
+    return /^[a-zA-Z]+(?:\s[a-zA-Z]+)*$/.test(input);
+}
