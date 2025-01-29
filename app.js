00f4f7831566d6eca2a6a877b1493921ea49767a
marcanojuan <marcano_juan@proton.me>
2025-01-29 06:48:11 -0400

    Implementación función actualizar lista de amigos

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
+const inputElement = document.getElementById('amigo');
+const listElement = document.getElementById('listaAmigos');
+
+// Validar solo letras y espacios con Regex.
+const inputIsValid = (input) => /^[a-zA-Z]+(?:\s[a-zA-Z]+)*$/.test(input);
+
 let friends = [];
 
-function addFriends() {
-    const elementInput = document.getElementById('amigo');
-    const friendName = elementInput.value.trim();
+function agregarAmigo() {
+    const friendName = inputElement.value.trim();
 
-    if (!isValidInput(friendName)) {
-        alert('Por favor, ingrese un nombre valido.');
+    if (!inputIsValid(friendName)) {
+        alertShow('Por favor, ingrese un nombre válido.');
         return;
     }
 
     friends.push(friendName);
-    elementInput.value = '';
-    updateFriendsList();
+    inputElement.value = '';
+    updateListFriends();
     return;
 }
 
-function isValidInput(input) {
-    // Regex para validar que el input sea un string con solo letras y espacios.
-    return /^[a-zA-Z]+(?:\s[a-zA-Z]+)*$/.test(input);
+function alertShow(message) {
+    alert(message);
+    return;
+}
+
+function updateListFriends() {
+    // Vaciar contenedor en forma segura.
+    while (listElement.firstChild) {
+        listElement.removeChild(listElement.firstChild);
+    }
+
+    // Crear fragmento y añadir elementos.
+    const fragment = document.createDocumentFragment();
+    friends.forEach((friend) => {
+        const elementLi = document.createElement('li');
+        elementLi.textContent = friend;
+        fragment.appendChild(elementLi);
+    });
+
+    // Actualizar DOM en una sola operación.
+    listElement.appendChild(fragment);
+    return;
 }
