cbbaff913945237e07752f069c8df6b7ee6ad625
marcanojuan <marcano_juan@proton.me>
2025-01-29 07:29:12 -0400

    Se agrega comentarios de documentación

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+// Capturar el elemento de campo de entrada.
 const inputElement = document.getElementById('amigo');
+
+// Obtener el elemento de lista.
 const listElement = document.getElementById('listaAmigos');
 
-// Validar solo letras y espacios con Regex.
+// Validar con Regex la entrada donde solo se aceptan letras y espacios.
 const inputIsValid = (input) => /^[a-zA-Z]+(?:\s[a-zA-Z]+)*$/.test(input);
 
-let friends = [];
+let amigos = [];
 
 function agregarAmigo() {
+    // Obtener el valor del campo de entrada y limpiar espacios en blanco.
     const friendName = inputElement.value.trim();
 
     if (!inputIsValid(friendName)) {
@@ -14,8 +18,12 @@ function agregarAmigo() {
         return;
     }
 
-    friends.push(friendName);
+    // Actualizar arreglo de amigos.
+    amigos.push(friendName);
+
+    // Limpiar campo de entrada.
     inputElement.value = '';
+
     updateListFriends();
     return;
 }
@@ -26,20 +34,23 @@ function alertShow(message) {
 }
 
 function updateListFriends() {
-    // Vaciar contenedor en forma segura.
+    // Limpiar lista existente en forma segura.
     while (listElement.firstChild) {
         listElement.removeChild(listElement.firstChild);
     }
 
-    // Crear fragmento y añadir elementos.
+    // Crear fragmento para mejorar rendimiento.
     const fragment = document.createDocumentFragment();
-    friends.forEach((friend) => {
+    
+    // Iterar sobre arreglo para crear elementos.
+    amigos.forEach((friend) => {
         const elementLi = document.createElement('li');
         elementLi.textContent = friend;
+        // Agregar elemento al fragmento.
         fragment.appendChild(elementLi);
     });
 
-    // Actualizar DOM en una sola operación.
+    // Agregar fragmento a la lista en el DOM en una sola operación.
     listElement.appendChild(fragment);
     return;
 }
